8b464eea906c35d033641f649d2a4ad66fbd19b1
fbahle <info@funfried.de>
2024-04-03 11:25:00 +0200

    Initial commit

---
+++ src/main/java/de/funfried/maven/plugin/zonky/utils/AlreadyStartedPolicy.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 Airtango.
+ * All rights reserved.
+ */
+
+package de.funfried.maven.plugin.zonky.utils;
+
+/**
+ *
+ * @author fbahle
+ */
+public enum AlreadyStartedPolicy {
+	fail, reinit, ignore;
+}
